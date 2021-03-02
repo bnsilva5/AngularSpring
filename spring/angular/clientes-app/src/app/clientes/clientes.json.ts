be9f8f012bf6750d852e257cefd7fe5494e2fd32
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-03-02 00:08:52 -0500

    Datos de los clientes en formato JSON

---
+++ spring/angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Nicholas', apellido: 'Silva', email: 'bnsilva5@gmail.com', createAt: '01-03-2021'},
+  {id: 2, nombre: 'MordeCay', apellido: 'Pepito', email: 'mordecay@gmail.com', createAt: '02-03-2021'},
+  {id: 3, nombre: 'Geraldine', apellido: 'Murcia', email: 'geralmu@gmail.com', createAt: '03-03-2021'},
+  {id: 4, nombre: 'Juan', apellido: 'Pine', email: 'juanpine16@gmail.com', createAt: '04-03-2021'},
+  {id: 5, nombre: 'Mayim', apellido: 'Bialik', email: 'bialikmayim@gmail.com', createAt: '05-03-2021'},
+  {id: 6, nombre: 'Sheldon', apellido: 'Cooper', email: 'coopersheldon3@gmail.com', createAt: '06-03-2021'},
+  {id: 7, nombre: 'Leonard', apellido: 'Hofstadter', email: 'hofstadterleonar64@gmail.com', createAt: '01-03-2021'},
+];
