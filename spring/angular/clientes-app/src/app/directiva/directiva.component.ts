d418743ed3128ed2d0d796f86a380fd8053b7ab0
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-03-01 21:06:17 -0500

    Ejemplo de ngIf y ngFor

---
+++ spring/angular/clientes-app/src/app/directiva/directiva.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+
+export class DirectivaComponet {
+  listaCurso: string[] = ['Typescript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+
+  habilitar: boolean = true;
+  constructor() { };
+
+  setHabilitar(): void {
+    this.habilitar = (this.habilitar==true)? false: true;
+  };
+}
