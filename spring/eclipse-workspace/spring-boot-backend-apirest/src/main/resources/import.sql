6f45c92827e0ec3a1c0ea10767506ed69cc88770
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-03-09 00:53:38 -0500

    Datos de prueba

---
+++ spring/eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Nicholas', 'Silva', 'bnsilva5@gmail.com', '2021-03-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Geraldine', 'Murcia', 'geral@gmail.com', '2021-05-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Manuel', 'Silva', 'masr@gmail.com', '2021-06-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Juan', 'Gatillazo', 'juan_g@gmail.com', '2021-04-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Nicol', 'Paine', 'nicol_pain@gmail.com', '2021-01-06');
