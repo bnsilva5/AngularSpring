53751aeef4f8d5dc0665596141eb388bfe3bec3e
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-03-09 00:53:20 -0500

    Se importa HttpClientModule y HttpClient (unión B-F)

--- spring/angular/clientes-app/src/app/clientes/cliente.service.ts
+++ spring/angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs'; //-- Programacion reactiva
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndpoint:string = 'http://localhost:8090/api/clientes';
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable <Cliente[]>{
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndpoint);
   }
 }
