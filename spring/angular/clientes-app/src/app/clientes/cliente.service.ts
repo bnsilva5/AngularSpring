161acd3f994e392d35282480284edf7aeb65c03d
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-03-13 01:26:29 -0500

    HttpHeaders y función create

--- spring/angular/clientes-app/src/app/clientes/cliente.service.ts
+++ spring/angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
+//import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs'; //-- Programacion reactiva
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +10,17 @@ import { HttpClient } from '@angular/common/http';
 export class ClienteService {
 
   private urlEndpoint:string = 'http://localhost:8090/api/clientes';
+
+  private httpHeaders = new HttpHeaders({'ContentType': 'application/json'})
+
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable <Cliente[]>{
     //return of(CLIENTES);
     return this.http.get<Cliente[]>(this.urlEndpoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeaders})
   }
 }
