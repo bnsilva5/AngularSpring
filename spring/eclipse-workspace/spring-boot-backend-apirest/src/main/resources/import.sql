f84242883f2fde4c117bca9229cfbf5d313ced46
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-03-24 00:45:13 -0500

    Se agregan datos de prueba

--- spring/eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
+++ spring/eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -3,3 +3,14 @@ INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Geraldine', '
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Manuel', 'Silva', 'masr@gmail.com', '2021-06-06');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Juan', 'Gatillazo', 'juan_g@gmail.com', '2021-04-06');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Nicol', 'Paine', 'nicol_pain@gmail.com', '2021-01-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Maria', 'Ruiz', 'braindead@gmail.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Tony', 'Silva Ruiz', 'brandon.silva@bitsamericas.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Eric', 'Foresta', 'bnsilva5@misena.edu.co', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Bet', 'Ruiz', 'silvaruizbrandon@gmail.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Nicholas', 'Draven', 'forestatony@gmail.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Nathalie', 'Silva C', 'nathalie@false.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rick', 'Allen', 'rick@true.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Joe', 'Elliot', 'joee123@def.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Milo', 'Rod', 'rodmilo@gmail.com', '2021-03-23');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('James', 'Hatfield', 'james@metallica.com', '2021-03-23');
+
