5b35314a3b3e625368d6e6a032eae1831a43a7e9
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-03-31 01:11:03 -0500

    Mensajes de validación desde la entidad Back-End

--- spring/eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/nicholas/springboot/backend/apirest/models/entity/Cliente.java
+++ spring/eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/nicholas/springboot/backend/apirest/models/entity/Cliente.java
@@ -18,16 +18,16 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotEmpty
-	@Size(min=4, max=12)
+	@NotEmpty(message = "No puede estar vacio")
+	@Size(min=4, max=12, message = "La cantidad de caracteres debe estar entre 4 y 12")
 	@Column(nullable=false)
 	private String nombre;
 	
-	@NotEmpty
+	@NotEmpty(message = "No puede estar vacio")
 	private String apellido;
 	
-	@NotEmpty
-	@Email
+	@NotEmpty(message = "No puede estar vacio")
+	@Email(message = "No es una direccion de correo valida")
 	@Column(nullable=false, unique=true)
 	private String email;
 	
