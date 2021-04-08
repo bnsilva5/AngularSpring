36decdcb66210b3dea0419e86a944ca50bbe4c49
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-04-08 18:12:33 -0500

    Se agrega la url de la pagina del back-end

--- spring/angular/clientes-app/src/app/clientes/cliente.service.ts
+++ spring/angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -20,27 +20,26 @@ export class ClienteService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  getClientes(): Observable <Cliente[]>{
-    return this.http.get<Cliente[]>(this.urlEndpoint).pipe(
-      tap(response =>{
-        let clientes = response as Cliente[];
+  getClientes(page: number): Observable <any>{
+    return this.http.get<Cliente[]>(this.urlEndpoint + '/page/' + page).pipe(
+      tap((response:any) =>{
         console.log('ClienteService: tap 1');
-        clientes.forEach(cliente =>{
+        (response.content as Cliente[]).forEach(cliente =>{
           console.log(cliente.nombre);
         })
       }),
-      map(response=> {
-        let clientes = response as Cliente[];
-        return clientes.map(cliente =>{
+      map((response:any)=> {
+        (response.content as Cliente[]).map(cliente =>{
           cliente.nombre = cliente.nombre.toUpperCase();
           let datePipe = new DatePipe('es');
           //cliente.createAt = datePipe.transform(cliente.createAt, 'EEEE dd, MMMM yyyy'); //formatDate(cliente.createAt, 'dd-MM-yyyy', 'en-US');
           return cliente;
         });
+        return response;
       }),
       tap(response =>{
         console.log('ClienteService: tap 2');
-        response.forEach(cliente => {
+        (response.content as Cliente[]).forEach(cliente => {
           console.log(cliente.nombre);
         })
       })
