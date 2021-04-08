0d10db18b05915798c99d50919544c7cc1ffc16a
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-04-08 18:26:14 -0500

    Se implementa funcion paginador Back-End

--- spring/eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/nicholas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ spring/eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/nicholas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -3,6 +3,8 @@ package com.nicholas.springboot.backend.apirest.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,12 @@ public class ClienteServiceImpl implements IClienteService {
 	public List<Cliente> findAll() {
 		
 		return (List<Cliente>) clienteDao.findAll();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
 	}
 
 	@Override
