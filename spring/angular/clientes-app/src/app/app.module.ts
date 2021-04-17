61b362a36c47609f0c1e7e551289cb5adf0a83e2
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-04-17 18:48:13 -0500

    Se agrega el componente detalle

--- spring/angular/clientes-app/src/app/app.module.ts
+++ spring/angular/clientes-app/src/app/app.module.ts
@@ -17,6 +17,7 @@ import localeES from '@angular/common/locales/es-CO';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { DetalleComponent } from './clientes/detalle/detalle.component';
 
 
 registerLocaleData(localeES, 'es');
@@ -28,6 +29,7 @@ const routes: Routes = [
   {path: 'clientes/page/:page', component: ClientesComponent},
   {path: 'clientes/form', component: FormComponent},
   {path: 'clientes/form/:id', component: FormComponent},
+  {path: 'clientes/view/:id', component: DetalleComponent},
 ]
 
 @NgModule({
@@ -38,7 +40,8 @@ const routes: Routes = [
     DirectivaComponet,
     ClientesComponent,
     FormComponent,
-    PaginatorComponent
+    PaginatorComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
