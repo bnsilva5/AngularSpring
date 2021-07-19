9dea92836d98b63a6d3253604304b2bc28477bbc
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2021-07-19 11:28:14 -0500

    Se agrega modalService para abrir el detalle de un cliente

--- spring/angular/clientes-app/src/app/clientes/clientes.component.ts
+++ spring/angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import { ModalService } from './detalle/modal.service';
 import swal from 'sweetalert2';
 import { tap } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
@@ -13,8 +14,10 @@ export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
   paginador: any;
+  clienteSeleccionado:Cliente;
 
   constructor(private clienteService: ClienteService,
+  private modalService : ModalService,
   private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
@@ -69,5 +72,10 @@ export class ClientesComponent implements OnInit {
           )
         }
       })
+  }
+
+  abrirModal(cliente:Cliente) {
+    this.clienteSeleccionado = cliente;
+    this.modalService.abrirModal();
   }
 }
